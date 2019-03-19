7bf2ebd1c6f18b5979388a92a21bb3b7f00daa3b
rodnavarroc <rodolfo.0926@hotmail.com>
2019-03-19 14:20:24 -0600

    Inicio del Proyecto

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HGCPI FIME | Inicio</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans|Oswald" rel="stylesheet">
+	<link rel="shortcut icon" type="image/png" href="favicon.ico"/> 
+	<meta charset="utf-8">
+</head>
+<body>
+	
+	<div class="col-md-12">
+		<div class="col-md-2"><center><img src="http://stechnology.org/2017/media/sponsor-images/FIMElogo-01.png" style="width: 80%;"></center></div>
+
+		<div class="col-md-8" style="padding-top: 3%; background-color: #215b33; padding-bottom: 3%;">
+		<center><h1 class="main_title">Herramienta General de Convocatorias para los Profesores-Investigadores de la FIME</h1></center>
+		</div>
+		
+		<div class="col-md-2"><br><center><img src="https://www.axondigital.mx/wp-content/uploads/2016/09/uanl.png" style="width: 80%;"></center></div>
+	</div>
+
+	<div class="col-md-12">
+		<div class="col-md-2"></div>
+		<div class="col-md-8">
+		
+		<div class="menu_main" style="padding-bottom: 3%; padding-top: 1%; background-color: #215b33;">
+		<div class="col-md-3" id="menu_element"><center><a href="">PRODEP</a></center></div>
+		<div class="col-md-3" id="menu_element"><center><a href="">SNI</a></center></div>
+		<div class="col-md-3" id="menu_element"><center><a href="">Estímulos</a></center></div>
+		<div class="col-md-3" id="menu_element"><center><a href="">Captura de Datos Personales</a></center></div>
+		</div>
+			
+		<br>
+		<p style="background-color: white; padding: 2% 2%; text-align: justify;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+
+		<div class="col-md-6">
+			<p style="text-align: justify;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>
+
+			<div class="col-md-6">
+			<p style="text-align: justify;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>
+
+		</div>
+		<div class="col-md-2">
+			<div id="lateral_info">
+				<center>
+					<br>
+					<h5>Datos del Investigador</h5>
+					<br>
+					<img src="https://dummyimage.com/400x400/000000/ffffff&text=Foto+del+Investigador" width="70%;"><br><br>
+					<p><b>Nombre: </b>Mayra Deyanira Flores Guerrero</p>
+					<p><b>Título: </b>Doctora en Educación con especialidad en Mecatrónica</p>
+					<p><b>Dependencia: </b> Facultad de Ingeniería Mecánica y Eléctrica</p>
+					<p><b>Cargo actual: </b> Docente | Jefa del Depto. de Sistemas de la FIME</p>
+				</center>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
